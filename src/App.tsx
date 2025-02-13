3d30fc018a8e7abc364c581da4f0b3931a7ea6d9
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-02-13 11:08:45 -0300

    style: adding the variables to be used in a standard way

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
   return (
-    <>
-    </>
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
+      <div className="pt-5 md:pt-7 px-8 sm:px-12 md:px-20 lg:px-24 xl:px-28 2xl:px-36">
+
+      </div>
+
+    </div>
   )
 }
 

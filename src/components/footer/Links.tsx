5047119aa78d93ff337e0534f3cb9c072a0862e4
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-02-17 16:05:53 -0300

    feat: Links component to redirect in footer

---
+++ src/components/footer/Links.tsx
@@ -0,0 +1,14 @@
+import { ImagTitleProps } from "../../types/ImgTitleProps";
+
+export function Links({ img, text, link }: ImagTitleProps) {
+    return (
+        <a href={link} className="flex justify-center pt-3 hover:opacity-75 items-center" target="_blank">
+            {img && (
+                <img src={img} alt="Icon" className="mr-2" />
+            )}
+            <p className="text-center text-[0.8rem] sm:text-[1rem] font-semibold text-text">
+                {text}
+            </p>
+        </a>
+    )
+}

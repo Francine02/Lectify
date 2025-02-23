94cfa6d7133d493dbbd13d3f97298cb1673c4577
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-02-23 13:35:24 -0300

    feat: added the link, from next, to redirect to their respective pages

--- src/components/home/Card.tsx
+++ src/components/home/Card.tsx
@@ -1,8 +1,9 @@
-import { ImagTitleProps } from "../../types/ImgTitleProps";
+import Link from "next/link";
+import { CardProps } from "../../types/ImgTitleProps";
 
-export function Card({ text, img }: ImagTitleProps) {
+export function Card({ text, img, href }: CardProps) {
     return (
-        <button className="pt-5 grid items-center justify-center hover:brightness-[96%] cursor-pointer transition duration-100 ease-in-out m-auto sm:m-0">
+        <Link href={href} className="pt-5 grid items-center justify-center hover:brightness-[96%] cursor-pointer transition duration-100 ease-in-out m-auto sm:m-0">
             <div className="rounded-3xl bg-gradient-to-t from-rosa to-bege p-[0.1rem] hover:from-bege hover:to-rosa transition duration-100 ease-in-out">
                 <div className="grid items-center justify-center bg-bg-components p-4 rounded-3xl w-60 md:w-72 h-32 sm:h-40">
                     <img src={img} alt="Icon" className="mx-auto" />
@@ -12,7 +13,7 @@ export function Card({ text, img }: ImagTitleProps) {
                     </p>
                 </div>
             </div>
-        </button>
+        </Link>
 
     )
 }
--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -14,8 +14,8 @@ export function Hero() {
             <SelectOption title={t('hero.page1.option')} />
 
             <div className="sm:flex gap-5 md:gap-8 lg:gap-10 justify-center">
-                <Card text={t('hero.page1.summary')} img="/assets/Icon-resumo.png" />
-                <Card text={t('hero.page1.quiz')} img="/assets/Icon-quiz.png" />
+                <Card href="/summary" text={t('hero.page1.summary')} img="/assets/Icon-resumo.png" />
+                <Card href="/quiz" text={t('hero.page1.quiz')} img="/assets/Icon-quiz.png" />
             </div>
         </>
     )

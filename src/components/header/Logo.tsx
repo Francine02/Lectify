de8e262b0be5eb73bebca3d0dd86baa57dc0f75b
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-02-23 13:34:39 -0300

    feat: added Link, from next, to redirect on click

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export function Logo() {
     return (
-        <>
+        <Link href="/">
             <img
                 className='w-24 md:w-28 pt-2'
                 src="/assets/Logo-header.png"
                 alt="Logo escrito: Lectify." />
-        </>
+        </Link>
     )
 }

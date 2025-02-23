f3888eb81de517451af5d5b97b689203da22e70f
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-02-23 13:32:45 -0300

    refactor: renamed file to be home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
+import Head from "next/head"
 import { Hero } from "../components/home/Hero"
 
 function Home() {
   return (
     <>
+      <Head>
+        <title>Lectify | Home</title>
+      </Head>
       <Hero />
     </>
   )

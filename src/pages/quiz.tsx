37caadc85573d18f040820e970548c3eb3818c4b
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-02-23 14:19:23 -0300

    feat: quiz page

---
+++ src/pages/quiz.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head"
+import { Hero } from "../components/quiz/Hero"
+
+function Quiz() {
+    return (
+        <>
+            <Head>
+                <title>Lectify | Quiz</title>
+            </Head>
+            <Hero />
+        </>
+    )
+}
+
+export default Quiz

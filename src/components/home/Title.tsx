9442a62fa115556d17be9dea461d2a605418e476
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-02-23 14:17:09 -0300

    style: change in padding

--- src/components/home/Title.tsx
+++ src/components/home/Title.tsx
@@ -2,7 +2,7 @@ import { TitleProps } from "../../types/TitleProps"
 
 export function Title({ title, emphasis, titleContinuation }: TitleProps) {
     return (
-        <h1 className="text-title-phone sm:text-title-tablet md:text-title-md lg:text-title-lg 2xl:text-title-2x1 text-center font-black text-text drop-shadow-xl pb-10">
+        <h1 className="text-title-phone sm:text-title-tablet md:text-title-md lg:text-title-lg 2xl:text-title-2x1 text-center font-black text-text drop-shadow-xl pb-6">
             {title}
             <span className="bg-gradient-to-r from-rosa via-rosa-secundary to-bege bg-[length:100%_0.5rem] bg-no-repeat bg-bottom pb-1"> {emphasis}
             </span>
--- src/components/summary/InputUrl.tsx
+++ src/components/summary/InputUrl.tsx
@@ -9,16 +9,16 @@ export function InputUrl() {
                 <div className="m-3 p-[0.11rem] rounded-full bg-gradient-to-t from-rosa via-rosa-secundary to-bege">
                     <label htmlFor="name" className="sr-only">Name</label>
                     <input
-                        className="p-1.5 md:p-2 placeholder:p-2.5 w-full rounded-full bg-bg-components focus:outline-none"
+                        className=" pl-5 pr-20 py-2 w-full rounded-full bg-bg-components focus:outline-none"
                         type="text"
                         id="name"
                         placeholder={t('hero.page2.url')}
                     />
                 </div>
-                <div className="absolute top-1 md:top-1.5 right-5 p-0.5 rounded-2xl bg-gradient-to-t from-[#FF005B] to-[#F2D3AC]">
+                <div className="absolute h-full top-0 right-3 p-0.5 rounded-full bg-gradient-to-t from-[#FF005B] to-[#F2D3AC]">
                     <button
                         type="button"
-                        className="flex items-center rounded-2xl bg-white py-1 px-2.5 text-center text-sm text-black transition-all shadow-sm hover:shadow cursor-pointer hover:opacity-90 active:opacity-85">
+                        className="flex h-full items-center rounded-full bg-white py-1 px-4 text-center text-sm font-semibold transition-all shadow-sm hover:shadow cursor-pointer hover:opacity-90 active:opacity-85">
                         {t('hero.page2.button')}
                     </button>
                 </div>

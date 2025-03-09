c1b54717e912c853c96e36e1bf4110f91e73117b
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-03-09 15:48:52 -0300

    feat: validation and error message

--- src/components/summary/InputUrl.tsx
+++ src/components/summary/InputUrl.tsx
@@ -1,7 +1,16 @@
+import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
+import { Button } from "./Button";
 
 export function InputUrl() {
     const { t } = useTranslation();
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        mode: "onBlur"
+    });
+
+    const onSubmit = () => {
+        console.log('foi')
+    }
 
     return (
         <div className="w-full sm:w-96 md:w-[30rem] m-auto mb-4">
@@ -9,18 +18,32 @@ export function InputUrl() {
                 <div className="m-3 p-[0.11rem] rounded-full bg-gradient-to-t from-rosa via-rosa-secundary to-bege">
                     <label htmlFor="name" className="sr-only">Name</label>
                     <input
+                        {...register('url', {
+                            required: "A URL é obrigatória!",
+                            maxLength: {value: 200, message: "Tamanho da URL excedido!"},
+                            pattern: {
+                                value: /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=|embed\/|v\/|.+\?v=)?([^&]+)/,
+                                message: "Url inválida!"
+
+                            }
+                        })}
                         className=" pl-5 pr-20 py-2 w-full rounded-full bg-bg-components focus:outline-none"
                         type="text"
                         id="name"
                         placeholder={t('hero.page2.url')}
                     />
                 </div>
+                {errors.url &&
+                    <div className=" absolute top-12 left-4 text-sm font-bold flex items-center gap-2">
+                        <img src="./assets/info.png" alt="Alerta de erro" className="w-4 h-4"/>
+                        <p className="text-red-500">
+                            {errors?.url.message?.toString()}
+                        </p>
+                    </div>
+                }
+
                 <div className="absolute h-full top-0 right-3 p-0.5 rounded-full bg-gradient-to-t from-[#FF005B] to-[#F2D3AC]">
-                    <button
-                        type="button"
-                        className="flex h-full items-center rounded-full bg-white py-1 px-4 text-center text-sm font-semibold transition-all shadow-sm hover:shadow cursor-pointer hover:opacity-90 active:opacity-85">
-                        {t('hero.page2.button')}
-                    </button>
+                    <Button onClick={() => handleSubmit(onSubmit)()} text={t('hero.page2.button')} />
                 </div>
             </div>
         </div>

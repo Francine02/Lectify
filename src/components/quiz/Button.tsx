38177a75537c03bc26cd2660b329725def7a46ff
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-03-14 18:20:05 -0300

    feat: modified to implement loading and other props

--- src/components/quiz/Button.tsx
+++ src/components/quiz/Button.tsx
@@ -1,15 +1,24 @@
-import { useTranslation } from "react-i18next";
+import { Loading } from "./Loading";
 
-export function Button() {
-    const { t } = useTranslation();
+interface ButtonProps {
+    onClick: (e: React.FormEvent) => void,
+    loading: boolean,
+    text: string
+}
+
+export function Button({ onClick, loading, text }: ButtonProps) {
     return (
-        <div className="pt-5 grid items-center justify-center hover:brightness-[96%] cursor-pointer transition duration-100 ease-in-out m-auto sm:m-0">
+        <div className="pt-5 grid items-center justify-center hover:brightness-[96%] cursor-pointer transition duration-100 ease-in-out m-auto sm:m-0" onClick={onClick}>
             <div className="rounded-xl bg-gradient-to-t from-rosa to-bege p-[0.1rem] hover:from-bege hover:to-rosa transition duration-100 ease-in-out">
                 <div className="grid items-center justify-center bg-bg-components px-14 py-1.5 rounded-xl w-20">
 
-                    <p className="text-center text-[0.8rem] sm:text-[1rem] font-bold text-text">
-                    {t("hero.page3.button")}
-                    </p>
+                    {loading ? (
+                        <Loading />
+                    ) : (
+                        <p className="text-center text-[0.8rem] sm:text-[1rem] font-bold text-text">
+                            {text}
+                        </p>
+                    )}
                 </div>
             </div>
         </div>

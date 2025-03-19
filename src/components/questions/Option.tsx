3621cd8ecc945b2a7f7750c4365fd2e827c19db2
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-03-19 12:20:19 -0300

    feat: Option component with question alternatives + dynamic color change + justification

---
+++ src/components/questions/Option.tsx
@@ -0,0 +1,33 @@
+import { useTranslation } from "react-i18next";
+
+interface OptionProps {
+    alternative: string,
+    selected: string,
+    onClick: () => void,
+    response: string,
+    justification: string
+}
+
+export function Option({ alternative, selected, onClick, response, justification }: OptionProps) {
+    const { t } = useTranslation();
+    const isSelectedCorrect = selected === response && selected === alternative;
+    const isSelectedWrong = selected !== response && selected === alternative;
+    const isSelected = selected.length > 1 && response === alternative;
+
+    return (
+        <>
+            <button
+                disabled={selected.length > 1}
+                onClick={onClick}
+                className={`${isSelectedCorrect ? 'bg-green-200' : isSelectedWrong ? 'bg-red-200' : 'bg-bg-components'} ${isSelected ? 'bg-green-200 cursor-text pointer-events-none' : selected.length > 1 && 'cursor-text pointer-events-none'} bg-bg-components w-full md:w-[35rem] max-h-44 overflow-y-auto rounded-lg p-2 px-8 mx-auto mb-4 cursor-pointer`}>
+                <p>{alternative}</p>
+                {isSelected &&
+                    <p className="text-justify mt-5 font-medium">
+                        <span className="font-black">{t('hero.page4.justification')} </span>
+                        {justification}
+                    </p>}
+            </button>
+
+        </>
+    )
+}

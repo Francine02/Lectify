6a17ff11763959b10693227f5635cd3216d35e1e
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-03-19 12:08:30 -0300

    feat: page with questions + page with scores

---
+++ src/pages/finishQuiz.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head"
+import { Hero } from "../components/finishQuiz/Hero"
+
+function FinishQuiz() {
+    return (
+        <>
+            <Head>
+                <title>Lectify | Finish Quiz</title>
+            </Head>
+            <Hero />
+        </>
+    )
+}
+
+export default FinishQuiz
---
+++ src/pages/questions.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head"
+import { Hero } from "../components/questions/Hero"
+
+function Questions() {
+    return (
+        <>
+            <Head>
+                <title>Lectify | Questions</title>
+            </Head>
+            <Hero />
+        </>
+    )
+}
+
+export default Questions

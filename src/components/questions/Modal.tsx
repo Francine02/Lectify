a2929c77a51dc258120269872255c1e99741498e
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-03-19 12:19:38 -0300

    feat: modal component, allowing any content inside(children)

---
+++ src/components/questions/Modal.tsx
@@ -0,0 +1,30 @@
+import { Dispatch, SetStateAction } from "react"
+
+interface ModalProps {
+    setShowModal: Dispatch<SetStateAction<boolean>>
+    children: React.ReactNode
+}
+
+export function Modal({ setShowModal, children }: ModalProps) {
+    return (
+        <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+
+            <div className="fixed inset-0 bg-gray-500/75 transition-opacity" aria-hidden="true"></div>
+
+            <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                <div className="flex min-h-full items-center justify-center p-4 text-center">
+                    <div className="relative transform overflow-hidden rounded-lg bg-white shadow-xl transition-all max-w-lg">
+                        <p onClick={() => setShowModal(false)} className="flex justify-end pr-5 pt-2 text-gray-500 cursor-pointer">x</p>
+                        <div className="bg-white px-5 pb-8 ">
+                            <div className="sm:flex sm:items-start">
+                                <div>
+                                    {children}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}

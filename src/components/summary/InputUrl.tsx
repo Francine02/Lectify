ff99a2391db931e6861caf9f61a5049a89e519ed
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-03-19 12:09:20 -0300

    feat: added loading and button operation

--- src/components/summary/InputUrl.tsx
+++ src/components/summary/InputUrl.tsx
@@ -1,16 +1,10 @@
-import { useForm } from "react-hook-form";
+import { useFormContext } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { Button } from "./Button";
 
-export function InputUrl() {
+export function InputUrl({onClick, loading}: {onClick: () => void, loading: boolean}) {
     const { t } = useTranslation();
-    const { register, handleSubmit, formState: { errors } } = useForm({
-        mode: "onBlur"
-    });
-
-    const onSubmit = () => {
-        console.log('foi')
-    }
+    const { register, formState: { errors } } = useFormContext();
 
     return (
         <div className="w-full sm:w-96 md:w-[30rem] m-auto mb-4">
@@ -18,7 +12,7 @@ export function InputUrl() {
                 <div className="m-3 p-[0.11rem] rounded-full bg-gradient-to-t from-rosa via-rosa-secundary to-bege">
                     <label htmlFor="name" className="sr-only">Name</label>
                     <input
-                        {...register('url', {
+                        {...register('youtube_url', {
                             required: "A URL é obrigatória!",
                             maxLength: {value: 200, message: "Tamanho da URL excedido!"},
                             pattern: {
@@ -43,7 +37,7 @@ export function InputUrl() {
                 }
 
                 <div className="absolute h-full top-0 right-3 p-0.5 rounded-full bg-gradient-to-t from-[#FF005B] to-[#F2D3AC]">
-                    <Button onClick={() => handleSubmit(onSubmit)()} text={t('hero.page2.button')} />
+                    <Button onClick={onClick} loading={loading} text={t('hero.page2.button')} />
                 </div>
             </div>
         </div>

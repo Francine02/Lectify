fa1910899173245467d9ef29fbcd255b48c54ba5
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-03-19 12:10:36 -0300

    feat: button operation, loading and style changes

--- src/components/quiz/Button.tsx
+++ src/components/quiz/Button.tsx
@@ -3,13 +3,14 @@ import { Loading } from "./Loading";
 interface ButtonProps {
     onClick: (e: React.FormEvent) => void,
     loading: boolean,
-    text: string
+    text: string,
+    disabled?: boolean
 }
 
-export function Button({ onClick, loading, text }: ButtonProps) {
+export function Button({ onClick, loading, text, disabled }: ButtonProps) {
     return (
-        <div className="pt-5 grid items-center justify-center hover:brightness-[96%] cursor-pointer transition duration-100 ease-in-out m-auto sm:m-0" onClick={onClick}>
-            <div className="rounded-xl bg-gradient-to-t from-rosa to-bege p-[0.1rem] hover:from-bege hover:to-rosa transition duration-100 ease-in-out">
+        <button disabled={disabled} className={`${disabled ? 'cursor-not-allowed' : 'cursor-pointer hover:brightness-[96%] '} pt-5 grid items-center justify-center transition duration-100 ease-in-out mx-auto sm:m-0`} onClick={onClick}>
+            <div className={`${disabled ? '' : 'hover:from-bege hover:to-rosa transition duration-100 ease-in-out'} rounded-xl bg-gradient-to-t from-rosa to-bege p-[0.1rem] `}>
                 <div className="grid items-center justify-center bg-bg-components px-14 py-1.5 rounded-xl w-20">
 
                     {loading ? (
@@ -21,7 +22,7 @@ export function Button({ onClick, loading, text }: ButtonProps) {
                     )}
                 </div>
             </div>
-        </div>
+        </button>
 
     )
 }

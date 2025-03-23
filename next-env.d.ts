8b4e46f91863c19a93ddb6613b90258394d8f758
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-03-23 17:20:12 -0300

    doc: ignoring .env and typing to Vercel

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,14 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/pages/api-reference/config/typescript for more information.
+
+interface ImportMetaEnv {
+    readonly VITE_EMAILJS_SERVICE_ID: string;
+    readonly VITE_EMAILJS_TEMPLATE_ID: string;
+    readonly VITE_EMAILJS_PUBLIC_KEY: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+  

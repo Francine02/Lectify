edffdd5056b1979aed4423d9beadb0e127767dce
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-03-23 17:40:17 -0300

    fix: name of environment variables

--- next-env.d.ts
+++ next-env.d.ts
@@ -5,9 +5,9 @@
 // see https://nextjs.org/docs/pages/api-reference/config/typescript for more information.
 
 interface ImportMetaEnv {
-    readonly VITE_EMAILJS_SERVICE_ID: string;
-    readonly VITE_EMAILJS_TEMPLATE_ID: string;
-    readonly VITE_EMAILJS_PUBLIC_KEY: string;
+    readonly NEXT_PUBLIC_EMAILJS_SERVICE_ID: string;
+    readonly NEXT_PUBLIC_EMAILJS_TEMPLATE_ID: string;
+    readonly NEXT_PUBLIC_EMAILJS_PUBLIC_KEY: string;
   }
   
   interface ImportMeta {

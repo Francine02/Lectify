4dc8f88f9c605e6e07e6074cab1a3e1614cf354d
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-03-23 17:28:48 -0300

    style :modified the color, rounded and added more props

--- src/components/quiz/Button.tsx
+++ src/components/quiz/Button.tsx
@@ -2,26 +2,26 @@ import { Loading } from "./Loading";
 
 interface ButtonProps {
     onClick: (e: React.FormEvent) => void,
-    loading: boolean,
+    loading?: boolean,
     text: string,
-    disabled?: boolean
+    disabled?: boolean,
+    secondary?: boolean,
+    className?: string
 }
 
-export function Button({ onClick, loading, text, disabled }: ButtonProps) {
+export function Button({ onClick, loading, text, disabled, secondary, className }: ButtonProps) {
     return (
-        <button disabled={disabled} className={`${disabled ? 'cursor-not-allowed' : 'cursor-pointer hover:brightness-[96%] '} pt-5 grid items-center justify-center transition duration-100 ease-in-out mx-auto sm:m-0`} onClick={onClick}>
-            <div className={`${disabled ? '' : 'hover:from-bege hover:to-rosa transition duration-100 ease-in-out'} rounded-xl bg-gradient-to-t from-rosa to-bege p-[0.1rem] `}>
-                <div className="grid items-center justify-center bg-bg-components px-14 py-1.5 rounded-xl w-20">
+        <button disabled={disabled} className={`${className ? className : 'text-center text-[0.8rem] sm:text-[1rem] font-bold px-14 py-1.5 rounded-lg w-20'}  relative grid items-center justify-center transition duration-100 ease-in-out
+                ${secondary ? 'cursor-not-allowed bg-bg-components border-[0.1rem] border-gray-300' : 'bg-black text-white before:bg-[linear-gradient(90deg,#ff4242,#a1ff42,#42a1ff,#42d0ff,#a142ff)] before:bottom-[-3px] before:left-0 before:opacity-60 before:absolute before:inset-0 before:-z-50 before:blur-md before:w-full before:h-full before:rounded-xl'}
+                ${disabled ? 'cursor-not-allowed' : secondary ? 'cursor-pointer hover:brightness-[96%]' : 'hover:bg-[#1c1c1c] cursor-pointer'} `} onClick={onClick}>
 
-                    {loading ? (
-                        <Loading />
-                    ) : (
-                        <p className="text-center text-[0.8rem] sm:text-[1rem] font-bold text-text">
-                            {text}
-                        </p>
-                    )}
-                </div>
-            </div>
+            {loading ? (
+                <Loading />
+            ) : (
+                <p>
+                    {text}
+                </p>
+            )}
         </button>
 
     )

ac13b39b88fdfa9d8a0c735ee61d8fc64d570ca8
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-03-23 17:23:21 -0300

    feat: buttons, one to open the modal and one to return to the home page

--- src/components/finishQuiz/Hero.tsx
+++ src/components/finishQuiz/Hero.tsx
@@ -1,16 +1,40 @@
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Subtitle } from "../questions/Subtitle";
+import { Button } from "../quiz/Button";
 import { ConfettiDemo } from "./Confetti";
+import { Feedback } from "./Feedback";
+import { PointerDemo } from "./Pointer";
 import { Score } from "./Score";
 
 export function Hero() {
     const { t } = useTranslation();
+    const router = useRouter();
+    const [showModal, setShowModal] = useState(false);
+
+    const handleBack = () => {
+        router.push('/')
+        sessionStorage.clear();
+    }
 
     return (
-        <section className="text-center">
+        <section className="text-center bg-bg-components max-w-[35rem] mx-auto p-10 rounded-lg">
+            <img src="/assets/congratulations.png" alt="Felicidades icone" className="mx-auto w-20"/>
             <ConfettiDemo />
             <Subtitle title={t('hero.page5.points')} />
-            <Score/>
+            <Score />
+            <div className=" pt-10 flex justify-center gap-4">
+                <Button secondary onClick={handleBack} text={t('hero.page5.back')} />
+
+                <div>
+                    <PointerDemo />
+                    <Button onClick={() => setShowModal(true)} text={t('hero.page5.evaluate')} />
+                </div>
+            </div>
+            {showModal && (
+                <Feedback showModal={setShowModal} />
+            )}
         </section>
     )
 }

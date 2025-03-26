8c15df97cc05c14f012e9d94265036d00fb8ceb2
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-03-26 09:37:36 -0300

    feat: error handling based on status code

--- src/service/quizRequest.ts
+++ src/service/quizRequest.ts
@@ -1,14 +1,28 @@
 import axios from "./config"
 
-export const quizRequest = async (formData: FormData) => {    
-    try  {
-        const response  = await axios.post('questions', formData);
+export const quizRequest = async (formData: FormData) => {
+    try {
+        const response = await axios.post('questions', formData);
 
         return {
             data: response.data,
             code: response.status
         }
-    } catch (error) {
-        return { error: "Erro ao converter arquivo!" };
+    } catch (error: any) {
+        if (error.response) {
+            const { status, data } = error.response;
+
+            if (status === 413) {
+                return { error: "file size exceeds the maximum limit of 5 mb." };
+            }
+
+            if (status === 400) {
+                return { error: data.error };
+            }
+
+            return { error: "Processing error" };
+        }
+
+        return { error: "Network error" };
     }
 }
---
+++ src/types/DataQuiz.ts
@@ -0,0 +1,4 @@
+export interface DataQuiz {
+    file: File | null,
+    quizPolity: boolean
+}

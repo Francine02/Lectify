621a2a5df3e252cff46eb2f9aa253c30a1c47929
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-03-26 09:24:16 -0300

    style: modified background for better accessibility when button is disabled

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -5,7 +5,7 @@ export function Button({ onClick, loading, text, disabled, secondary, className 
     return (
         <button disabled={disabled} className={`${className ? className : 'text-center text-[0.8rem] sm:text-[1rem] font-bold px-14 py-1.5 rounded-lg w-20'}  relative grid items-center justify-center transition duration-100 ease-in-out
                 ${secondary ? 'cursor-not-allowed bg-bg-components border-[0.1rem] border-gray-300' : 'bg-black text-white before:bg-[linear-gradient(90deg,#ff4242,#a1ff42,#42a1ff,#42d0ff,#a142ff)] before:bottom-[-3px] before:left-0 before:opacity-60 before:absolute before:inset-0 before:-z-50 before:blur-md before:w-full before:h-full before:rounded-xl'}
-                ${disabled ? 'cursor-not-allowed' : secondary ? 'cursor-pointer hover:brightness-[96%]' : 'hover:bg-[#1c1c1c] cursor-pointer'} `} onClick={onClick}>
+                ${disabled ? 'cursor-not-allowed bg-gray-400' : secondary ? 'cursor-pointer hover:brightness-[96%]' : 'hover:bg-[#1c1c1c] cursor-pointer'} `} onClick={onClick}>
 
             {loading ? (
                 <Loading />

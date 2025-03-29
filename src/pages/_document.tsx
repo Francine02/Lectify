ed8a26634218074df023c6087287bdc4650cbb0d
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-03-29 17:21:48 -0300

    feat: adSense script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,8 @@ export default function Document() {
                 <title>Lectify</title>
                 <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet" />
                 <link rel="icon" type="image/png" href="/assets/logo-rosa.png" />
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1044585829409035"
+                    crossOrigin="anonymous"></script>
             </Head>
             <body>
                 <Main />

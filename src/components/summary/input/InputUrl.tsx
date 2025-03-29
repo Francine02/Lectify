1aec29c8fc8fb175d41c78ea7868b3dd3ecffea8
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-03-29 19:15:10 -0300

    refactor: changing to use i18n on errors

--- src/components/summary/input/InputUrl.tsx
+++ src/components/summary/input/InputUrl.tsx
@@ -1,11 +1,10 @@
 import { useFormContext } from "react-hook-form";
 import { useTranslation } from "react-i18next";
-import { Error } from "../../error/Error";
 import { Button } from "../../button/Button";
 
 export function InputUrl({ onClick, loading }: { onClick: () => void, loading: boolean }) {
     const { t } = useTranslation();
-    const { register, formState: { errors, isValid } } = useFormContext();
+    const { register, formState: {  isValid } } = useFormContext();
 
     return (
         <div className="max-w-80 sm:max-w-full sm:w-96 md:w-[30rem] m-auto mb-4">
@@ -13,11 +12,11 @@ export function InputUrl({ onClick, loading }: { onClick: () => void, loading: b
 
                 <input
                     {...register('youtube_url', {
-                        required: "A URL é obrigatória!",
-                        maxLength: { value: 200, message: "Tamanho da URL excedido!" },
+                        required: t('errors.type.required'),
+                        maxLength: { value: 200, message: t('errors.URL exceeds maximum length') },
                         pattern: {
                             value: /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=|embed\/|v\/|.+\?v=)?([^&]+)/,
-                            message: "Url inválida!"
+                            message: t('errors.URL invalid')
 
                         }
                     })}
@@ -25,9 +24,6 @@ export function InputUrl({ onClick, loading }: { onClick: () => void, loading: b
                     type="text"
                     placeholder={t('hero.page2.url')}
                 />
-                {errors.youtube_url &&
-                    <Error text={errors.youtube_url?.message?.toString()} />
-                }
 
                 <div className="absolute top-0.5 right-1 p-[0.1rem]">
                     <Button disabled={!isValid} className="h-7.5 items-center rounded-lg py-1 px-3 text-center text-sm font-bold " onClick={onClick} loading={loading} text={t('hero.page2.button')} />

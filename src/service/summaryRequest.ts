273b33d91ab4b997cf07cd667a2897c7155c3ebe
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-03-29 17:35:30 -0300

    feat: request to generate summary

--- src/service/summaryRequest.ts
+++ src/service/summaryRequest.ts
@@ -1,15 +1,26 @@
 import { DataSummarize } from "../types/DataSummarize";
-import axios from "./config"
+import axios from "axios";
 
 export const summaryRequest = async (data: DataSummarize) => {    
     try  {
-        const response  = await axios.post('summarize', data);
+        const response  = await axios.post(`${process.env.NEXT_PUBLIC_LECTIFY_API_SUMMARY}`, data, { responseType: 'blob' });
+
+        const downloadUrl = URL.createObjectURL(response.data); 
 
         return {
-            data: response.data,
-            error: response.statusText
+            data: downloadUrl,
+            code: response.status,
         }
-    } catch (error) {
-        return { error: "Erro ao converter arquivo!" };
+    } catch (error: any) {
+        if (error.response) {
+            const { status, data } = error.response;
+
+            if (status === 400) {
+                console.log(error)
+                return { error: data.error };
+            }
+        }
+        console.log(error)
+        return { error: "Processing error" };
     }
 }

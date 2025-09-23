eecd59b3d99f6fc9d0dd2d3f403bf03923f1fe58
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-09-23 20:40:35 -0300

    feat (button): improve loading/disabled state handling and accessibility

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -5,8 +5,13 @@ import { ButtonProps } from './ButtonProps';
 export function Button({ children, loading, className, ...props }: ButtonProps) {
   return (
     <button
+      aria-busy={loading}
+      disabled={loading || props.disabled}
       className={cn(
-        'w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-rose-700 text-white hover:brightness-95 focus:outline-hidden focus:bg-gray-300 disabled:bg-gray-300 disabled:pointer-events-none cursor-pointer',
+        'w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent text-white transition',
+        loading || props.disabled
+          ? 'bg-gray-400 focus:bg-gray-400 cursor-not-allowed opacity-70'
+          : 'bg-rose-700 hover:brightness-95 focus:brightness-110 cursor-pointer focus:outline-hidden',
         className
       )}
       {...props}

af4e387081cdabd941765e1b25342143ed6ba3d1
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-09-26 20:34:42 -0300

    feat (login): added useForm and login request

--- src/app/(public)/login/form.tsx
+++ src/app/(public)/login/form.tsx
@@ -1,17 +1,58 @@
-import { Button } from '@/components/Button';
-import { Divider } from '@/components/Divider';
+'use client';
 import { Input } from '@/components/Input';
+import { zodResolver } from '@hookform/resolvers/zod';
 import Link from 'next/link';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { LoginData, loginSchema } from './login-schema';
+import { Button } from '@/components/button';
+import { useState } from 'react';
+import { loginRequest } from '@/service/auth/login-request';
+import { Error } from '@/components/Error';
 
 export function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginData>({
+    resolver: zodResolver(loginSchema),
+  });
+  const [error, setError] = useState<string>();
+
+  const onSubmit: SubmitHandler<LoginData> = async (data) => {
+    setError('');
+    const result = await loginRequest(data);
+
+    if (!result.success) {
+      setError(result.error?.message);
+      return;
+    }
+  };
+
   return (
-    <>
+    <form className="space-y-2" onSubmit={handleSubmit(onSubmit)}>
       <h1 className="text-title font-black text-center mb-10 ">Login</h1>
-      <Input.Root label="Nome de usuário" placeholder="exemplo12" />
-      <Input.Password label="Senha" placeholder="*****" />
+      <Input.Root
+        {...register('username')}
+        errors={!!errors.username}
+        helperText={errors.username?.message}
+        label="Nome de usuário"
+        placeholder="exemplo12"
+      />
+      <Input.Password
+        {...register('password')}
+        errors={!!errors.password}
+        helperText={errors.password?.message}
+        label="Senha"
+        placeholder="*****"
+      />
+      {error && <Error text={error} />}
+
       <p className="text-gray-600 hover:underline cursor-pointer text-sm mt-1">Esqueceu a senha?</p>
 
-      <Button className="mt-10 md:mt-12 mb-4">Entrar</Button>
+      <Button loading={isSubmitting} className="mt-10 md:mt-12 mb-4" type="submit">
+        Entrar
+      </Button>
 
       <p className="text-gray-600 text-sm">
         Não possui uma conta?
@@ -22,6 +63,6 @@ export function LoginForm() {
           Cadastre-se
         </Link>
       </p>
-    </>
+    </form>
   );
 }

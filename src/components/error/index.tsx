67cb955f3a4ae6b128ed9c499a62bfc9086aecc5
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-09-26 20:35:15 -0300

    refactor (components): modified component to use cn and react icons

--- src/components/error/index.tsx
+++ src/components/error/index.tsx
@@ -1,12 +1,12 @@
-import { ErrorProps } from "./ErrorProps";
+import { cn } from '@/utils/cn';
+import { ErrorProps } from './ErrorProps';
+import { MdErrorOutline } from 'react-icons/md';
 
 export function Error({ text, className }: ErrorProps) {
-    return (
-        <div className={`text-sm font-bold flex items-center gap-2 ${className}`}>
-            <img src="/assets/info.png" alt="Alerta de erro" className="w-4 h-4" />
-            <p className="text-red-500">
-                {text}
-            </p>
-        </div>
-    )
+  return (
+    <div className={cn('text-xs md:text-sm mt-2 mb-2.5 flex items-center gap-2', className)}>
+      <MdErrorOutline className="text-red-600" />
+      <p className="text-red-600">{text}</p>
+    </div>
+  );
 }

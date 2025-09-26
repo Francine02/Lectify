dcd2caf187ab460e80c9fc7df01e8145e33ece09
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-09-26 20:42:54 -0300

    feat (components): added `IndicatorStrongPassword` which shows password strength in dash format

---
+++ src/components/StrongPassword/IndicatorStrongPasword/index.tsx
@@ -0,0 +1,14 @@
+export function IndicatorStrongPassword() {
+  return (
+    <div
+      id="hs-strong-password"
+      data-hs-strong-password='{
+            "target": "#hs-strong-password-with-indicator-and-hint",
+            "hints": "#hs-strong-password-hints",
+            "stripClasses": "hs-strong-password:opacity-100 hs-strong-password-accepted:bg-teal-500 h-2 flex-auto rounded-full bg-blue-700 opacity-50 mx-1",
+            "minLength": "8"
+          }'
+      className="flex mt-2 -mx-1"
+    ></div>
+  );
+}

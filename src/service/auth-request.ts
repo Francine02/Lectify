e8cab2d008ec459823cd56f9fc7578129f685f79
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-09-26 20:38:45 -0300

    feat (service): added `auth-request` with reusable function for authenticating requests

---
+++ src/service/auth-request.ts
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { BASE_URL } from './config/axios-config';
+
+export type ApiResponse<T = any> = {
+  success: boolean;
+  data?: T;
+  error?: {
+    code: string;
+    message: string;
+  };
+};
+
+export async function authRequest<T>(
+  method: 'post',
+  endpoint: string,
+  data?: any
+): Promise<ApiResponse<T>> {
+  try {
+    const response = await axios({
+      method,
+      url: `${BASE_URL}${endpoint}`,
+      data,
+    });
+
+    return {
+      success: true,
+      data: response.data,
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      error: {
+        code: error.code,
+        message: error.response.data.error,
+      },
+    };
+  }
+}

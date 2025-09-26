ef31f9e17b0e8b8df137685f46a3dcf99113bfff
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-09-26 20:30:59 -0300

    feat (register): added request for registration and useForm

--- src/app/(public)/cadastro/form.tsx
+++ src/app/(public)/cadastro/form.tsx
@@ -1,26 +1,55 @@
 'use client';
-import { Button } from '@/components/Button';
+import { Button } from '@/components/button';
 import { Input } from '@/components/Input';
+import { checkEmailRequest } from '@/service/auth/check-email-request';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'next/navigation';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { RegisterData, registerSchema } from './register-schema';
 
 export function RegisterForm() {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm<RegisterData>({
+    resolver: zodResolver(registerSchema),
+  });
   const router = useRouter();
 
-  const handleSubmit = () => {
+  const onSubmit: SubmitHandler<RegisterData> = async (data) => {
+    const result = await checkEmailRequest(data);
+
+    if (!result.success) {
+      setError('email', {
+        type: 'manual',
+        message: result.error?.message,
+      });
+      return;
+    }
+
+    sessionStorage.setItem('email', data.email);
     router.push('/cadastro/verificar-codigo');
   };
 
   return (
-    <>
+    <form className="space-y-2" onSubmit={handleSubmit(onSubmit)}>
       <p className="text-sm text-center text-gray-500 mb-10">
         Insira seu email para receber o código de verificação.
       </p>
 
-      <Input.Root label="Email" placeholder="exemplo@gmail.com" />
+      <Input.Root
+        {...register('email')}
+        errors={!!errors.email}
+        helperText={errors.email?.message}
+        label="Email"
+        placeholder="exemplo@gmail.com"
+      />
 
-      <Button className="mt-10 md:mt-12 mb-4" onClick={() => handleSubmit()}>
+      <Button className="mt-10 md:mt-12 mb-4" type="submit" loading={isSubmitting}>
         Enviar
       </Button>
-    </>
+    </form>
   );
 }

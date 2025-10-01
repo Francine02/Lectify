f1eb50a72035159c15118f43f4763af8221e116a
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-09-30 22:18:31 -0300

    refactor (service): changed the imports to use aliases

--- src/service/auth/login-request.ts
+++ src/service/auth/login-request.ts
@@ -1,4 +1,4 @@
-import { LoginData } from 'app/(public)/login/login-schema';
+import { LoginData } from '@/app/(public)/login/login-schema';
 import { ApiResponse, authRequest } from '../auth-request';
 
 export const loginRequest = async (data: LoginData): Promise<ApiResponse> => {
--- src/service/auth/register-request.ts
+++ src/service/auth/register-request.ts
@@ -1,4 +1,4 @@
-import { PersonalInformationData } from 'app/(public)/cadastro/informacoes-pessoais/personal-information-schema';
+import { PersonalInformationData } from '@/app/(public)/cadastro/informacoes-pessoais/personal-information-schema';
 import { ApiResponse, authRequest } from '../auth-request';
 
 export const registerRequest = async (data: PersonalInformationData): Promise<ApiResponse> => {

682f0ae055d3937081f56927ffa88a12fc522a91
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-09-30 22:14:53 -0300

    delete (register): removed registration scheme

--- src/app/(public)/cadastro/register-schema.ts
+++
@@ -1,9 +0,0 @@
-import z from 'zod';
-
-export const registerSchema = z.object({
-  email: z
-    .email({ message: 'Email inválido' })
-    .max(130, 'Email precisa ser menor que 130 caracteres'),
-});
-
-export type RegisterData = z.infer<typeof registerSchema>;

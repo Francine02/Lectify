948b4787b7bf1bc136363d3ae0e1187975a2a048
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-09-30 22:27:00 -0300

    feat (register): added step validation and resend code feature to check code flow

--- src/app/(public)/cadastro/verificar-codigo/form.tsx
+++ src/app/(public)/cadastro/verificar-codigo/form.tsx
@@ -1,11 +1,14 @@
 'use client';
 import { Button } from '@/components/button';
 import { Input } from '@/components/Input';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useRouter } from 'next/navigation';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { CheckCodeData, checkCodeSchema } from './check-code-schema';
+import { ResendCode } from '@/components/ResendCode';
+import { useResendCode } from '@/hooks/useResendCode';
 import { checkEmailRequest } from '@/service/auth/check-email-request';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { redirect, useRouter } from 'next/navigation';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { CheckCodeFormInputs, checkCodeSchema } from 'schemas/code-schema';
+import { setStep } from '../auth-step';
 
 export function CheckCodeForm() {
   const {
@@ -13,29 +16,33 @@ export function CheckCodeForm() {
     handleSubmit,
     setError,
     formState: { errors, isSubmitting },
-  } = useForm<CheckCodeData>({
+  } = useForm<CheckCodeFormInputs>({
     resolver: zodResolver(checkCodeSchema),
   });
   const router = useRouter();
+  const email = sessionStorage.getItem('email') ?? '';
 
-  const onSubmit: SubmitHandler<CheckCodeData> = async (data) => {
-    const email = sessionStorage.getItem('email') ?? '';
+  const { timer, handleResend } = useResendCode({ email, isSubmitting });
+
+  const onSubmit: SubmitHandler<CheckCodeFormInputs> = async (data) => {
+    const code = `${data.input1}${data.input2}${data.input3}${data.input4}${data.input5}${data.input6}`;
 
     const payload = {
       email,
-      ...data,
+      code,
     };
 
     const result = await checkEmailRequest(payload);
 
     if (!result.success) {
-      setError('code', {
+      setError('root', {
         type: 'manual',
         message: result.error?.message,
       });
       return;
     }
 
+    await setStep(2);
     router.push('/cadastro/informacoes-pessoais');
   };
 
@@ -47,11 +54,11 @@ export function CheckCodeForm() {
 
       <Input.Code
         register={register}
-        errors={!!errors.code}
-        helperText={errors.code?.message}
+        errors={!!errors.input1}
+        helperText={errors.input1?.message}
         label="Código de verificação"
-        maxLength={6}
       />
+      <ResendCode timer={timer} onResend={handleResend} />
 
       <Button className="mt-10 md:mt-12 mb-4" type="submit" loading={isSubmitting}>
         Avançar
--- src/app/(public)/cadastro/verificar-codigo/page.tsx
+++ src/app/(public)/cadastro/verificar-codigo/page.tsx
@@ -1,6 +1,13 @@
+import { cookies } from 'next/headers';
 import { CheckCodeForm } from './form';
+import { redirect } from 'next/navigation';
 
-function CheckCode() {
+async function CheckCode() {
+  const cookieStore = await cookies();
+  const step = cookieStore.get('step')?.value;
+
+  if (step !== '1') redirect('/cadastro');
+
   return <CheckCodeForm />;
 }
 

ede08047f5e2327a9217d2618cad550a20c9d1cc
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-10-02 19:36:56 -0300

    refactor (service): changed to receive code and email

--- src/service/auth/check-code-request.ts
+++ src/service/auth/check-code-request.ts
@@ -1,5 +1,10 @@
 import { authRequest, ApiResponse } from '../auth-request';
 
-export const checkCodeRequest = (code: string): Promise<ApiResponse> => {
-  return authRequest('post', '/verify_email_register', { code });
+type CodeRequest = {
+  code: string;
+  email: string;
+};
+
+export const checkCodeRequest = (data: CodeRequest): Promise<ApiResponse> => {
+  return authRequest('post', '/verify_email_register', data);
 };

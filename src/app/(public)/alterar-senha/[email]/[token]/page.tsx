6531fd951a7e51229761a61843c2d4d3b80501b5
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-10-02 19:29:01 -0300

    feat (change-password): added page with form to change password

---
+++ src/app/(public)/alterar-senha/[email]/[token]/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from 'next';
+import { ChangePasswordForm } from './form';
+
+export const metadata: Metadata = {
+  title: 'Alterar a senha | Lectify',
+  description: 'Insira sua nova senha para fazer login',
+  robots: { index: false, follow: false },
+};
+
+export default async function ChangePasswordPage() {
+  return <ChangePasswordForm />;
+}

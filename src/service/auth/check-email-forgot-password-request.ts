3fc9a5cef527aa456e142e5344214820a2354f21
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-10-02 19:37:26 -0300

    feat (service): added request to verify email and change password

---
+++ src/service/auth/check-email-forgot-password-request.ts
@@ -0,0 +1,11 @@
+import { ApiResponse, authRequest } from '../auth-request';
+
+export const checkEmailForgotPasswordRequest = (email: string): Promise<ApiResponse> => {
+  const payload = {
+    email,
+    base_url: process.env.NEXT_PUBLIC_URL,
+    reset_password_page_url: 'alterar-senha',
+  };
+
+  return authRequest('post', '/ping_check_email_reset_password', payload);
+};

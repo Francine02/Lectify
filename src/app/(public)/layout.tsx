59733f9fdafc0daea1d9fcd68a027acfeedb54f0
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-10-03 17:40:37 -0300

    style: changed responsiveness and added Logo

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,14 +1,17 @@
+import { Logo } from '@/components/Logo';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="grid grid-cols-1 md:grid-cols-2 h-screen overflow-auto">
-      <section className="order-2 md:order-1 mx-auto md:m-auto w-full px-5 max-w-md 2x1:max-w-lg">
+      <section className="order-2 md:order-1 m-auto w-full px-5 max-w-md 2x1:max-w-lg py-6">
         {children}
       </section>
 
-      <section className="bg-purple-700 flex-end h-30 md:h-screen order-1 md:order-2">
+      <section className="relative bg-purple-700 order-1 md:order-2 h-36 md:h-full">
+        <Logo className="absolute right-5 lg:right-10 top-6 z-10" />
         <img
           src="/assets/background.jpeg"
-          className="w-full h-full object-cover md:object-contain object-left"
+          className="absolute inset-0 w-full h-full md:object-fill object-cover"
         />
       </section>
     </main>

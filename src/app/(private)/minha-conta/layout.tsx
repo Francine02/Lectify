f805f5ed4d69c08bd8d4a42fdfe9c1749f306277
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-10-24 19:44:00 -0300

    feat (my-account): added my account layout

---
+++ src/app/(private)/minha-conta/layout.tsx
@@ -0,0 +1,19 @@
+import { AccountHeader } from '@/components/MyAccount/AccountHeader';
+import { UploadImage } from '@/components/UploadImage';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="w-full">
+      <h1 className="text-title font-semibold border-b border-gray-200 w-full mb-2">Meu perfil</h1>
+
+      <section className="grid grid-cols-1 sm:grid-cols-2 gap-5 pt-5">
+        <div className="flex-col mx-auto text-center">
+          <AccountHeader />
+
+          <UploadImage />
+        </div>
+        {children}
+      </section>
+    </div>
+  );
+}

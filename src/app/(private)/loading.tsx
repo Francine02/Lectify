61f59d9685bbb486ccbffb4ee4ae32f8390c8e11
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-10-24 19:42:08 -0300

    feat (app): added loading page

---
+++ src/app/(private)/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-white">
+      <div className="animate-spin rounded-full h-10 w-10 border-4 border-purple-700 border-t-transparent"></div>
+    </div>
+  );
+}

ff2396c3cd67ff0269d144ed3854c8c5f0f9aa23
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-10-27 19:34:27 -0300

    feat (service): `base-request.ts` to be used in private routes

---
+++ src/service/base-request.ts
@@ -0,0 +1,31 @@
+import { ApiResponse } from '@/types/ApiResponse';
+import api, { BASE_URL } from './config/axios-config';
+
+export async function baseRequest<T>(
+  method: 'post' | 'put' | 'patch' | 'get' | 'delete',
+  endpoint: string,
+  data?: any,
+  config: object = {}
+): Promise<ApiResponse<T>> {
+  try {
+    const response = await api({
+      method,
+      url: `${BASE_URL}${endpoint}`,
+      data,
+      ...config,
+    });
+
+    return {
+      success: true,
+      data: response.data,
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      error: {
+        code: error.code,
+        message: error.response?.data?.error,
+      },
+    };
+  }
+}

7d733212d2f44bdd5b628fbe96dff018feebfb95
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-10-27 18:53:57 -0300

    refactor (register): changed schema to merge profileSchema and passwordSchema

--- src/app/(public)/cadastro/informacoes-pessoais/personal-information-schema.ts
+++ src/app/(public)/cadastro/informacoes-pessoais/personal-information-schema.ts
@@ -1,25 +1,7 @@
 import { passwordSchema } from 'schemas/password-schema';
+import { profileSchema } from 'schemas/profile-schema';
 import z from 'zod';
 
-export const personalInformationSchema = passwordSchema.extend({
-  username: z
-    .string()
-    .min(3, 'Nome de usuário precisa ter pelo menos 3 caracteres')
-    .max(32, 'Nome de usuário precisa ser menor que 32 caracters')
-    .regex(
-      /^[a-zA-Z0-9_]+$/,
-      'Nome de usuário precisa ter somente letras, números e sublinhado (_)'
-    ),
-  firstname: z
-    .string()
-    .min(3, 'Nome precisa ter pelo menos 3 caracteres')
-    .max(32, 'Nome precisa ser menor que 32 caracteres')
-    .regex(/^[A-Za-z]{1,32}$/, 'Nome precisa conter somente letras'),
-  lastname: z
-    .string()
-    .min(3, 'Sobrenome precisa ter pelo menos 3 caracteres')
-    .max(32, 'Sobrenome precisa ser menor que 32 caracteres')
-    .regex(/^[A-Za-z]{1,32}$/, 'Sobrenome precisa conter somente letras'),
-});
+export const personalInformationSchema = profileSchema.merge(passwordSchema);
 
 export type PersonalInformationData = z.infer<typeof personalInformationSchema>;

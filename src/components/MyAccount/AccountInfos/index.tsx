d7d2e9f3f399e96bf17f65aaa8ae5f4d5d5ea45d
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-10-27 19:07:19 -0300

    feat (MyAccount): `AccountInfos` with user information such as name, account creation and email

---
+++ src/components/MyAccount/AccountInfos/index.tsx
@@ -0,0 +1,30 @@
+'use client';
+import { Loading } from '@/components/Loading';
+import { fieldsAccount } from '@/constants/form/fields-account';
+import { getInformationItem } from '@/utils/informations/get-informations';
+import { useEffect, useState } from 'react';
+
+export function AccountInfos() {
+  const [info, setInfo] = useState<InformationsAccountType | null>(null);
+
+  useEffect(() => {
+    setInfo({
+      username: String(getInformationItem('username') ?? '-'),
+      created: String(getInformationItem('created_at') ?? '-'),
+      email: String(getInformationItem('email') ?? '-'),
+    });
+  }, []);
+
+  if (!info) return <Loading />;
+
+  return (
+    <div className="flex flex-wrap justify-between items-center gap-5 text-sm border-t sm:border-t-0 border-b border-gray-200 py-5 sm:pt-0">
+      {fieldsAccount(info).map((field) => (
+        <div key={field.label}>
+          <p className="font-semibold">{field.label}:</p>
+          <p className="font-normal">{field.value}</p>
+        </div>
+      ))}
+    </div>
+  );
+}

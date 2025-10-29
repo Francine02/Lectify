8bcd3f475ab5835ab3bdaad3a948938567d32eda
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-10-29 16:35:20 -0300

    style (components): adjusted width, alignment, and shadow

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,7 +3,7 @@ import { cn } from '@/utils/cn';
 export function Logo({ className }: { className?: string }) {
   return (
     <>
-      <img className={cn('w-10 ', className)} src="/assets/logo.png" alt="Logo" />
+      <img className={cn('w-8 sm:w-10 ', className)} src="/assets/logo.png" alt="Logo" />
     </>
   );
 }
--- src/components/Sidebar/SidebarItem/index.tsx
+++ src/components/Sidebar/SidebarItem/index.tsx
@@ -10,10 +10,10 @@ export function SidebarItem({ link, icon: Icon, name }: SidebarItemProps) {
   const isActive = path === link;
 
   return (
-    <div className="hs-tooltip [--placement:right] block w-full ml-10">
+    <div className="hs-tooltip relative [--placement:right] block w-full ml-[1.35rem] drop-shadow-sm">
       <Link
         className={cn(
-          'hs-tooltip-toggle flex h-9.5 items-center p-2  text-sm font-semibold rounded-lg border border-transparent text-white hover:bg-gray-100 hover:text-purple-700 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none',
+          'hs-tooltip-toggle flex h-9.5 items-center p-2 text-sm font-semibold rounded-l-lg border border-transparent text-white hover:bg-gray-100 hover:text-purple-700 focus:outline-hidden focus:bg-gray-100 focus:text-purple-700 disabled:opacity-50 disabled:pointer-events-none transition-colors',
           isActive && 'bg-gray-100 text-purple-700'
         )}
         href={link}

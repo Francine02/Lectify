eefebcffc75e0949b464ac7fd0da9ead15b3e49a
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-10-29 16:27:01 -0300

    feat (app): added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import PrelineScriptWrapper from '@/components/Preline/PrelineScriptWrapper';
 import '@/style/globals.css';
 import { Lora } from 'next/font/google';
 import '@/style/globals.css';
+import { Footer } from '@/components/Footer';
 
 const lora = Lora({
   subsets: ['latin'],
@@ -12,8 +13,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="pt-BR" className={lora.className}>
       <body>
-        {children}
-        <PrelineScriptWrapper />
+        <div className="flex flex-col min-h-screen">
+          <main className="flex-1">
+            {children}
+            <PrelineScriptWrapper />
+          </main>
+
+          <Footer />
+        </div>
       </body>
     </html>
   );

cec836459e759bdcc3aca1ac5e57147df46b55df
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-10-29 16:38:09 -0300

    feat (components): added `Footer` with developers, logo, and contact links

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,21 @@
+import { Logo } from '../Logo';
+import { Contacts } from './Contacts';
+import { Developers } from './Developers';
+
+export function Footer() {
+  return (
+    <footer className="bg-black text-[#dadada] py-10 px-4 sm:px-10 md:px-20 lg:px-34 w-full">
+      <div className="max-w-7xl mx-auto w-full flex flex-wrap justify-between items-start gap-3">
+        <Logo />
+
+        <Developers />
+
+        <Contacts />
+      </div>
+
+      <div className="max-w-7xl mx-auto mt-10 border-t border-[#7a7a7a] pt-4 text-center text-xs">
+        <p>&copy; 2024 Lectify. Todos os direitos reservados.</p>
+      </div>
+    </footer>
+  );
+}

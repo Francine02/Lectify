c0cfc427539e20f89514feab31216e6917922e5c
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-10-30 15:51:01 -0300

    refactor (Sidebar): hide sidebar open button in `/confirm-delete`

--- src/components/Sidebar/ButtonOpenSidebar/index.tsx
+++ src/components/Sidebar/ButtonOpenSidebar/index.tsx
@@ -1,17 +1,22 @@
+'use client';
+import { usePathname } from 'next/navigation';
 import { FaHamburger } from 'react-icons/fa';
 
 export function ButtonOpenSidebar() {
-  return (
-    <button
-      className="absolute top-5 left-4 sm:left-10 lg:hidden hover:opacity-85 cursor-pointer"
-      type="button"
-      aria-haspopup="dialog"
-      aria-expanded="false"
-      aria-controls="hs-sidebar-mini-sidebar"
-      aria-label="Toggle navigation"
-      data-hs-overlay="#hs-sidebar-mini-sidebar"
-    >
-      <FaHamburger className="text-purple-700 size-6" />
-    </button>
-  );
+  const path = usePathname();
+
+  if (!path.startsWith('/confirmar-exclusao'))
+    return (
+      <button
+        className="absolute top-5 left-4 sm:left-10 lg:hidden hover:opacity-85 cursor-pointer"
+        type="button"
+        aria-haspopup="dialog"
+        aria-expanded="false"
+        aria-controls="hs-sidebar-mini-sidebar"
+        aria-label="Toggle navigation"
+        data-hs-overlay="#hs-sidebar-mini-sidebar"
+      >
+        <FaHamburger className="text-purple-700 size-6" />
+      </button>
+    );
 }

95ac572d308c25f4f2ace5eb6fd1bb7acb2bc97c
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-12-22 18:59:47 -0300

    refactor (footer): change `contacts` to uppercase

--- src/components/Footer/Contacts/index.tsx
+++ src/components/Footer/Contacts/index.tsx
@@ -1,11 +1,11 @@
-import { contacts } from '@/constants/footer/contacts';
+import { CONTACTS } from '@/constants/footer/contacts';
 
 export function Contacts() {
   return (
     <nav aria-label="Contatos">
       <h3 className="text-normal sm:text-lg font-semibold">Contatos</h3>
       <ul className="mt-2 text-xs sm:text-sm gap-2 sm:space-x-3 grid grid-cols-2">
-        {contacts.map((contact) => {
+        {CONTACTS.map((contact) => {
           const Icon = contact.icon;
           return (
             <li key={contact.link}>

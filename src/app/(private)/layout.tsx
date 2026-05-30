9528c7132a850a87d3de5106341bb9561517bf0b
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-30 13:43:25 -0300

    style: change max-w for smaller size in 2XL

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="flex w-full sm:px-10 mx-auto sm:py-12 bg-gray-100 min-h-screen">
       <ButtonOpenSidebar />
       <Sidebar />
-      <main className="bg-white rounded-lg w-full px-4 py-4 sm:p-6 md:max-w-5xl lg:max-w-10/12 2xl:max-w-7xl sm:m-auto pb-12 sm:pb-6 ">
+      <main className="bg-white rounded-lg w-full px-4 py-4 sm:p-6 md:max-w-5xl lg:max-w-10/12 2xl:max-w-6xl sm:m-auto pb-12 sm:pb-6 ">
         <Toast />
         {children}
       </main>

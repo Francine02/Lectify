24aea990e00d769f77c93585ddb85c37e531eb08
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-30 13:46:36 -0300

    refactor (components): change the data type to be URL only.

--- src/components/SummaryGenerated/FileView/FileViewProps.ts
+++ src/components/SummaryGenerated/FileView/FileViewProps.ts
@@ -1,3 +1,3 @@
-import { SummaryGeneratedProps } from '../SummaryGeneratedProps';
-
-export type FileViewProps = Omit<SummaryGeneratedProps, 'hasSummary'>;
+export type FileViewProps = {
+    url?: string;
+};

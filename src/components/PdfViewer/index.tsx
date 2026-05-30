d3ff527dc8dc8a2d36cf6c5121f57d939ff7d3b3
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-30 13:50:05 -0300

    refactor (components): change href and target in PdfViewer

--- src/components/PdfViewer/index.tsx
+++ src/components/PdfViewer/index.tsx
@@ -1,15 +1,17 @@
-export function PdfViewer() {
-  return (
-    <div className="w-full">
-      <iframe src="/assets/teste.pdf" className="w-full min-h-[60vh]" title="Resumo gerado" />
+import { FileViewProps } from '../SummaryGenerated/FileView/FileViewProps';
 
-      <p className="mt-2 text-sm">
-        Se o PDF não abrir, você pode{' '}
-        <a href="/assets/teste.pdf" className="underline">
-          baixar o arquivo
-        </a>
-        .
-      </p>
-    </div>
-  );
+export function PdfViewer({ url }: FileViewProps) {
+    return (
+        <div className="w-full">
+            <iframe src={url} className="w-full min-h-[60vh]" title="Resumo gerado" />
+
+            <p className="mt-2 text-sm">
+                Se não abrir, você pode{' '}
+                <a target='_blank' href={url} className="underline">
+                    baixar o arquivo
+                </a>
+                .
+            </p>
+        </div>
+    );
 }

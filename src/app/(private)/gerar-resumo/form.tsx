a3ad9c6a7a2584b369b7602d6389d9ef8ebd75c2
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-30 13:45:08 -0300

    feat (summary): submit with long polling adapted with timeout and circular progress component.

--- src/app/(private)/gerar-resumo/form.tsx
+++ src/app/(private)/gerar-resumo/form.tsx
@@ -8,79 +8,130 @@ import { SUMMARY_OUTPUT_FORMATS } from '@/constants/form/summary-output-formats'
 import { generateSummary } from '@/service/summary/generate-summary';
 import { cn } from '@/utils/cn';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
 import { SummaryData, summarySchema } from './summary-schema';
+import { IconCircle } from '@/components/IconCircle';
+import { checkSummary } from '@/service/summary/check-summary';
+import { Progress } from '@/components/Progress';
 
 export function GenerateSummaryForm() {
-  const {
-    register,
-    setValue,
-    watch,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm<SummaryData>({
-    resolver: zodResolver(summarySchema),
-    defaultValues: {
-      output_format: SUMMARY_OUTPUT_FORMATS[0],
-    },
-  });
-  const watchFormat = watch('output_format');
+    const {
+        register,
+        setValue,
+        watch,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm<SummaryData>({
+        resolver: zodResolver(summarySchema),
+        defaultValues: {
+            output_format: SUMMARY_OUTPUT_FORMATS[0],
+        },
+    });
+    const watchFormat = watch('output_format');
 
-  const onSubmit: SubmitHandler<SummaryData> = async (data) => {
-    const result = await generateSummary(data);
+    const [data, setData] = useState<string>();
+    const [progress, setProgress] = useState<string>('0');
 
-    if (!result.success) {
-      toast.error(result.error?.message);
-      return;
-    }
+    const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-    toast.success('Resumo gerado!');
-  };
+    const onSubmit: SubmitHandler<SummaryData> = async (data) => {
+        const firstRequest = await generateSummary(data);
+        setProgress('10');
 
-  const hasSummary = true;
+        if (!firstRequest.success) {
+            toast.error(firstRequest.error?.message);
+            return;
+        }
 
-  return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="space-y-7 min-h-screen flex flex-col justify-center sm:min-h-fit"
-    >
-      <h1 className={cn('text-title font-black pb-3', hasSummary ? 'pl-10 sm:pl-0' : '')}>
-        Gere um resumo
-      </h1>
+        let count = 0;
+        let checkResult;
 
-      <div className="flex flex-col gap-20 lg:flex-row lg:justify-between">
-        <div className="space-y-7 w-full">
-          <Input.Root
-            {...register('youtube_url')}
-            errors={!!errors.youtube_url}
-            helperText={errors.youtube_url?.message}
-            label="URL do YouTube"
-            placeholder="https://www.youtube.com/***"
-            className="lg:max-w-lg"
-          />
-          <div className="flex gap-5 pb-3">
-            <Radio
-              {...register('output_format')}
-              checked={watchFormat === SUMMARY_OUTPUT_FORMATS[0]}
-              value={SUMMARY_OUTPUT_FORMATS[0]}
-              onChange={() => setValue('output_format', SUMMARY_OUTPUT_FORMATS[0])}
-              text="PDF"
-            />
-            <Radio
-              {...register('output_format')}
-              checked={watchFormat === SUMMARY_OUTPUT_FORMATS[1]}
-              onChange={() => setValue('output_format', SUMMARY_OUTPUT_FORMATS[1])}
-              value={SUMMARY_OUTPUT_FORMATS[1]}
-              text="Markdown"
-            />
-          </div>
-          <Button isLoading={isSubmitting} type="submit" className="sm:w-fit sm:h-8 ">
-            Gerar
-          </Button>
-        </div>
-        <SummaryGenerated />
-      </div>
-    </form>
-  );
+        while (count < 5) {
+            await delay(40000);
+
+            const check = await checkSummary(data);
+
+            const progressValue = 10 + ((count + 1) * 80) / 5;
+
+            setProgress(progressValue.toFixed(1));
+
+            if (check.data?.status === 'success') {
+                setProgress('90');
+                checkResult = true;
+                break;
+            }
+
+            if (check.error) {
+                setProgress('0');
+                toast.error(firstRequest.error?.message);
+                setData('');
+                break;
+            }
+
+            count++;
+        }
+
+        if (checkResult) {
+            const result = await generateSummary(data);
+
+            if (!result.success || !result.data) {
+                toast.error(result.error?.message);
+                return;
+            }
+
+            const downloadUrl = URL.createObjectURL(result.data);
+
+            if (downloadUrl) {
+                setProgress('100');
+
+                await delay(1000);
+                setData(downloadUrl);
+                toast.success('Resumo gerado!');
+            }
+        }
+    };
+
+    const hasSummary = true;
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-7 min-h-screen flex flex-col justify-center sm:min-h-fit">
+            <IconCircle type="summary" />
+            <h1 className={cn('text-title font-black pb-3', hasSummary ? 'pl-10 sm:pl-0' : '')}>Gere um resumo</h1>
+
+            <div className="flex flex-col gap-20 lg:flex-row lg:justify-evenly ">
+                <div className="space-y-7 w-full">
+                    <Input.Root
+                        {...register('youtube_url')}
+                        errors={!!errors.youtube_url}
+                        helperText={errors.youtube_url?.message}
+                        label="URL do YouTube"
+                        placeholder="https://www.youtube.com/***"
+                        className="lg:max-w-lg"
+                    />
+                    <div className="flex gap-5 pb-3">
+                        <Radio
+                            {...register('output_format')}
+                            checked={watchFormat === SUMMARY_OUTPUT_FORMATS[0]}
+                            value={SUMMARY_OUTPUT_FORMATS[0]}
+                            onChange={() => setValue('output_format', SUMMARY_OUTPUT_FORMATS[0])}
+                            text="PDF"
+                        />
+                        <Radio
+                            {...register('output_format')}
+                            checked={watchFormat === SUMMARY_OUTPUT_FORMATS[1]}
+                            onChange={() => setValue('output_format', SUMMARY_OUTPUT_FORMATS[1])}
+                            value={SUMMARY_OUTPUT_FORMATS[1]}
+                            text="Markdown"
+                        />
+                    </div>
+                    <Button disabled={isSubmitting} type="submit" className="sm:w-fit sm:h-8 ">
+                        Gerar
+                    </Button>
+                </div>
+                {isSubmitting ? <Progress value={progress} /> : <SummaryGenerated hasSummary={!!data} url={data} />}
+            </div>
+        </form>
+    );
 }

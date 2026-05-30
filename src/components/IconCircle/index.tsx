d545990a941f9a0b6ea27e0cb8cb02c5a908be5f
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-30 13:49:11 -0300

    feat (components): IconCircle with circular background and icon for summary and quiz

---
+++ src/components/IconCircle/index.tsx
@@ -0,0 +1,27 @@
+import { cn } from '@/utils/cn';
+import { FaFileAlt } from 'react-icons/fa';
+import { IconCircleProps, IconConfig } from './IconCircleProps';
+import { FaFileCircleQuestion } from 'react-icons/fa6';
+
+export function IconCircle({ type }: IconCircleProps) {
+    const iconStyle: Record<string, IconConfig> = {
+        summary: {
+            icon: FaFileAlt,
+            bg: 'bg-purple-50',
+            iconColor: 'text-purple-800',
+        },
+        quiz: {
+            icon: FaFileCircleQuestion,
+            bg: 'bg-indigo-50',
+            iconColor: 'text-indigo-800',
+        },
+    };
+
+    const { icon: Icon, bg, iconColor } = iconStyle[type];
+
+    return (
+        <div className={cn(bg, 'w-fit rounded-full p-5 mx-auto lg:mx-0')}>
+            <Icon className={cn(iconColor, 'size-10')} />
+        </div>
+    );
+}

47fcd3d6f00301120b1739a713e3890d02daf099
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-30 13:46:05 -0300

    fix (service): error message when responseType is blob to be transformed into json.

--- src/service/base-request.ts
+++ src/service/base-request.ts
@@ -2,31 +2,41 @@ import { ApiResponse } from '@/types/ApiResponse';
 import api, { BASE_URL } from './config/axios-config';
 
 export async function baseRequest<T>(
-  method: 'post' | 'put' | 'patch' | 'get' | 'delete',
-  endpoint: string,
-  data?: any,
-  config: object = {}
+    method: 'post' | 'put' | 'patch' | 'get' | 'delete',
+    endpoint: string,
+    data?: any,
+    config: object = {}
 ): Promise<ApiResponse<T>> {
-  try {
-    const response = await api({
-      method,
-      url: `${BASE_URL}${endpoint}`,
-      data,
-      ...config,
-    });
+    try {
+        const response = await api({
+            method,
+            url: `${BASE_URL}${endpoint}`,
+            data,
+            ...config,
+        });
 
-    return {
-      success: true,
-      data: response.data,
-    };
-  } catch (error: any) {
-    const errorMessage = error.response?.data?.error;
-    return {
-      success: false,
-      error: {
-        code: error.code,
-        message: errorMessage ?? 'Ocorreu um erro! Por favor, tente novamente mais tarde',
-      },
-    };
-  }
+        return {
+            success: true,
+            data: response.data,
+        };
+    } catch (error: any) {
+        let errorMessage = '';
+        
+        if (error.response?.data instanceof Blob) {
+            const text = await error.response.data.text();
+            const json = JSON.parse(text);
+
+            errorMessage = json.error;
+        } else {
+            errorMessage = error.response?.data?.error;
+        }
+
+        return {
+            success: false,
+            error: {
+                code: error.code,
+                message: errorMessage ?? 'Ocorreu um erro! Por favor, tente novamente mais tarde',
+            },
+        };
+    }
 }

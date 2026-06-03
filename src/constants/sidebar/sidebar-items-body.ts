8b1aaf2170028195574c928f3ed742b85ff888fe
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-06-03 19:42:06 -0300

    feat (constants): add the page to the sidebar to generate a quiz

--- src/constants/sidebar/sidebar-items-body.ts
+++ src/constants/sidebar/sidebar-items-body.ts
@@ -1,14 +1,20 @@
 import { FaFileAlt, FaHome } from 'react-icons/fa';
+import { FaFileCircleQuestion } from 'react-icons/fa6';
 
 export const SIDEBAR_ITEMS_BODY = [
-  {
-    link: '/',
-    icon: FaHome,
-    name: 'Home',
-  },
-  {
-    link: '/gerar-resumo',
-    icon: FaFileAlt,
-    name: 'Gerar resumo',
-  },
+    {
+        link: '/',
+        icon: FaHome,
+        name: 'Home',
+    },
+    {
+        link: '/gerar-resumo',
+        icon: FaFileAlt,
+        name: 'Gerar resumo',
+    },
+    {
+        link: '/gerar-quiz',
+        icon: FaFileCircleQuestion,
+        name: 'Gerar questões',
+    },
 ];
